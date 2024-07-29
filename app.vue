59a1082dc28530798bced43ee3059225e32f1772
Alberto Fernandez <albertofdzm@gmail.com>
2024-07-29 02:50:05 +0200

    ♻️ remove default layout and move its content to app
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
 <template>
   <NuxtLoadingIndicator />
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div id="top"></div>
+  <header
+    className="fixed left-0 top-0 z-10 h-12 w-full bg-gradient-to-br from-slate-800 to-gray-900 shadow-md shadow-black"
+  >
+    <div className="container mx-auto flex h-full max-w-5xl items-center px-4">
+      <h1 className="flex h-full items-center"></h1>
+    </div>
+  </header>
+  <div className="flex min-h-screen flex-col">
+    <main
+      className="mx-auto mt-16 w-full px-5 pt-4 sm:max-w-2xl md:max-w-3xl lg:max-w-4xl"
+    >
+      <NuxtPage />
+    </main>
+  </div>
 </template>
