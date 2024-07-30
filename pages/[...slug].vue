2619cbc84b22fa17b81424d40c37e0958ca1305f
Alberto Fernandez <albertofdzm@gmail.com>
2024-07-30 21:08:17 +0200

    ✨ add not found page
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,14 @@
 <template>
-  <ContentDoc v-slot="{ doc: post }">
-    <article>
-      <h1>{{ post.title }}</h1>
-      <ContentRenderer :value="post" />
-    </article>
+  <ContentDoc>
+    <template v-slot="{ doc: post }">
+      <article>
+        <h1>{{ post.title }}</h1>
+        <ContentRenderer :value="post" />
+      </article>
+    </template>
+
+    <template #not-found>
+      <h1>Página no encontrada</h1>
+    </template>
   </ContentDoc>
 </template>
