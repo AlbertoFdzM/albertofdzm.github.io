b362de4c7f3f15a3b9c469296410ce908ee50441
Alberto Fernandez <albertofdzm@gmail.com>
2024-08-03 16:14:16 +0200

    💄 fix header
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

--- app.vue
+++ app.vue
@@ -1,18 +1,20 @@
 <template>
   <NuxtLoadingIndicator />
   <div id="top"></div>
-  <header
-    className="fixed left-0 top-0 z-10 h-12 w-full bg-gradient-to-br from-slate-800 to-gray-900 shadow-md shadow-black"
-  >
-    <div className="container mx-auto flex h-full max-w-5xl items-center px-4">
-      <h1 className="flex h-full items-center">
-        <NuxtLink to="/">AlbertoFdzM</NuxtLink>
-      </h1>
-    </div>
+  <header class="fixed left-0 top-0 z-10 h-12 w-full">
+    <nav
+      class="navbar bg-gradient-to-br from-slate-800 to-gray-900 shadow-md shadow-black w-full"
+    >
+      <div class="container mx-auto flex h-full max-w-5xl items-center px-4">
+        <h1 class="flex h-full items-center">
+          <NuxtLink to="/">AlbertoFdzM</NuxtLink>
+        </h1>
+      </div>
+    </nav>
   </header>
-  <div className="flex min-h-screen flex-col">
+  <div class="flex min-h-screen flex-col">
     <main
-      className="mx-auto mt-16 w-full px-5 pt-4 sm:max-w-2xl md:max-w-3xl lg:max-w-4xl"
+      class="mx-auto mt-16 w-full px-5 pt-4 sm:max-w-2xl md:max-w-3xl lg:max-w-4xl"
     >
       <NuxtPage />
     </main>
