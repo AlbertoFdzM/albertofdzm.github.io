539bb323f3c7cd444fab6885e06dc55acf3d5f4a
Alberto Fernandez <albertofdzm@gmail.com>
2024-08-04 02:39:23 +0200

    💄 avoid header overlapping anchor points
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import tailwindTypography from "@tailwindcss/typography";
 import daisyui from "daisyui";
 
 export default <Partial<Config>>{
+  daisyui: {
+    themes: ["dark"],
+  },
   content: [
     "./nuxt.config.ts",
     "./app.vue",
@@ -10,8 +13,5 @@ export default <Partial<Config>>{
     "./layouts/**/*.vue",
     "./content/**/*.md",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [tailwindTypography, daisyui],
 };
