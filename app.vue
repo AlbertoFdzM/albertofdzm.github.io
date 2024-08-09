b9f126a4d3c77761397db83bd1413908464aac88
Alberto Fernandez <albertofdzm@gmail.com>
2024-08-09 21:17:37 +0200

    ✨ move header to a Header component and add make it hide on scroll down
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

--- app.vue
+++ app.vue
@@ -2,15 +2,7 @@
   <NuxtLoadingIndicator />
   <div class="min-h-screen flex flex-col">
     <div id="top"></div>
-    <header class="fixed left-0 top-0 z-10 w-full">
-      <nav class="navbar bg-base-100 shadow-md shadow-black">
-        <div class="navbar-start">
-          <NuxtLink class="btn btn-ghost text-xl text-primary" to="/"
-            ><h1>AlbertoFdzM</h1></NuxtLink
-          >
-        </div>
-      </nav>
-    </header>
+    <Header />
     <main class="mx-auto mt-16 w-full py-4 h-full flex-grow">
       <NuxtPage />
     </main>
---
+++ components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header
+    class="fixed left-0 top-0 z-10 w-full transition-transform"
+    :class="{ '-translate-y-full': isHidden }"
+  >
+    <nav class="navbar bg-base-100 shadow-md shadow-black">
+      <div class="navbar-start">
+        <NuxtLink class="btn btn-ghost text-xl text-primary" to="/"
+          ><h1>AlbertoFdzM</h1></NuxtLink
+        >
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts" setup>
+const isHidden = ref(false);
+
+if (import.meta.browser) {
+  let lastScrollPosition = window.scrollY;
+
+  window.addEventListener("scroll", (ev) => {
+    requestAnimationFrame(() => {
+      const newScrollPosition = window.scrollY;
+
+      const isScrollingDown = lastScrollPosition < newScrollPosition;
+
+      if (isScrollingDown && !isHidden.value) {
+        isHidden.value = true;
+      } else if (!isScrollingDown && isHidden.value) {
+        isHidden.value = false;
+      }
+
+      lastScrollPosition = newScrollPosition;
+    });
+  });
+}
+</script>
+
+<style></style>
