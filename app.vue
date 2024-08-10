67bed49e974588fc314fce614873e22213bb7bb4
Alberto Fernandez <albertofdzm@gmail.com>
2024-08-10 12:56:10 +0200

    ♻️ move layout to default nuxt layout
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

--- app.vue
+++ app.vue
@@ -1,11 +1,5 @@
 <template>
-  <NuxtLoadingIndicator />
-  <div class="min-h-screen flex flex-col">
-    <div id="top"></div>
-    <Header />
-    <main class="mx-auto mt-16 w-full py-4 h-full flex-grow">
-      <NuxtPage />
-    </main>
-    <Footer />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
