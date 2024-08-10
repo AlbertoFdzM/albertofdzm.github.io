2079d3fe129783f68ee779290e13748a0766e45a
Alberto Fernandez <albertofdzm@gmail.com>
2024-08-10 12:57:06 +0200

    ✨ add error page
    Signed-off-by: Alberto Fernandez <albertofdzm@gmail.com>

---
+++ error.vue
@@ -0,0 +1,33 @@
+<template>
+  <NuxtLayout>
+    <ContentWrapper>
+      <div class="prose">
+        <h1>{{ error.statusCode }}</h1>
+        <p>{{ message }}</p>
+        <p class="text-center">
+          <NuxtLink class="btn btn-primary" to="/">Volver a inicio</NuxtLink>
+        </p>
+      </div>
+    </ContentWrapper>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+import type { NuxtError } from "#app";
+
+const { error } = defineProps<{
+  error: NuxtError;
+}>();
+
+let message = "";
+
+switch (error.statusCode) {
+  case 404:
+    message = "Página no encontrada.";
+    break;
+  default:
+    message = "Hemos tenido un error inesperado.";
+}
+</script>
+
+<style></style>
